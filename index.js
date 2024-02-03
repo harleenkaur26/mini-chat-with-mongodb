b401e5c6f9cf411cff41ed5848335a1980d8edd8
Harleen Kaur Makhija <hkmakhija26@gmail.com>
2024-02-03 23:18:52 +0530

    worked on edit and delete route

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));   //for css
 app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));   
 
 
 main()
@@ -49,15 +51,37 @@ app.post("/chats", (req, res) => {
     });
     res.redirect("/chats");
 
-})
-
+});
 
 // edit route
 app.get("/chats/:id/edit", async (req, res) => {
     let {id} = req.params;
     let chat = await Chat.findById(id);
     res.render("edit.ejs", {chat});
-})
+});
+
+// Update route
+app.put("/chats/:id", async (req, res) => {
+    let {id} = req.params;
+    let {msg : newMsg} = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(
+        id, 
+        {msg : newMsg}, 
+        {runValidators : true, new : true }
+    );
+    console.log(updatedChat);
+    res.redirect("/chats");
+});
+
+
+// delete route
+app.delete("/chats/:id", async (req, res) => {
+    let {id} = req.params;
+    let deletedChat = await Chat.findByIdAndDelete(id);
+    console.log(deletedChat);
+    res.redirect("/chats")
+});
+
 
 
 
